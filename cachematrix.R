8822c6021bc5a7cb2bc1d20f9bc0caba5e947369
George <zeuz007@hotmail.com>
2015-04-26 23:04:00 +0100

    assigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Matrix inversion
 
 ## Write a short comment describing this function
 ## Creates a matrix that can cache its inverse
